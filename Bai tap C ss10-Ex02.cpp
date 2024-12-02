5d03479453fa73cb86e98406e8c00a633f005389
Zarck299 <honganhzarck@gmail.com>
2024-12-03 01:12:27 +0700

    Update Bai tap C ss10

--- Bai tap C ss10-Ex02.cpp
+++ Bai tap C ss10-Ex02.cpp
@@ -1,35 +1,22 @@
 #include<stdio.h>
-    int arr[] = {1,2,3,4,5};
-
-void bubbleSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-}
-
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
-
 int main() {
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int arr[] = {1,14,27,35,44,58};
     int n = sizeof(arr) / sizeof(arr[0]); 
     printf("Mang ban dau: \n");
-    printArray(arr, n);
-
-    bubbleSort(arr, n);
-
+    for ( int i =0; i < n; i++){
+        printf("%d\t", arr[i]);
+    }
+    for (int i = 1; i < arr[i]; i++);
+    int keyNumber = arr[i];
+    int j = i-1;
+    while ( j >= 0 && key < arr[j]){
+        arr[j+1] = arr[j];
+        j--;
+    }
+    printf("\n");
     printf("Mang sau khi sap xep theo thu tu tang dan: \n");
-    printArray(arr, n);
+    for ( int i = 0; i < n; i++);
+    printf("%d\t", arr[i]);
 
     return 0;
 }
